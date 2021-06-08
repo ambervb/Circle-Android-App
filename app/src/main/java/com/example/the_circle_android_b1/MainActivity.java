2b7747529b6edf577d4e6b42bd58486453598982
Niek <niekwijsman@yahoo.nl>
2021-06-08 14:58:24 +0200

    basic activities

---
+++ app/src/main/java/com/example/the_circle_android_b1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.the_circle_android_b1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.the_circle_android_b1.activities.CameraPreviewActivity;
+import com.example.the_circle_android_b1.activities.ChatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cameraPreviewActivity(View view){
+        Intent intent = new Intent(MainActivity.this, CameraPreviewActivity.class);
+        startActivity(intent);
+    }
+    public void chatActivity(View view){
+        Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+        startActivity(intent);
+    }
+
+}
