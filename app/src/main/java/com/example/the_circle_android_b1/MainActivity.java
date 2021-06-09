7e405b9593b312051e702113b68761507066d163
Amber <abezouwe@avans.nl>
2021-06-09 02:56:46 +0200

    Adds Camera functionality

--- app/src/main/java/com/example/the_circle_android_b1/MainActivity.java
+++ app/src/main/java/com/example/the_circle_android_b1/MainActivity.java
@@ -2,22 +2,166 @@ package com.example.the_circle_android_b1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.se.omapi.Session;
+
+import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import net.majorkernelpanic.streaming.Session;
+import net.majorkernelpanic.streaming.SessionBuilder;
+import net.majorkernelpanic.streaming.audio.AudioQuality;
+import net.majorkernelpanic.streaming.gl.SurfaceView;
+import net.majorkernelpanic.streaming.video.VideoQuality;
 
 import com.example.the_circle_android_b1.activities.CameraPreviewActivity;
 import com.example.the_circle_android_b1.activities.ChatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, net.majorkernelpanic.streaming.Session.Callback, SurfaceHolder.Callback{
+    private Button mButton1, mButton2;
+    private SurfaceView mSurfaceView;
+    private EditText mEditText;
     private Session mSession;
+
+    private final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_camera_preview);
+
+        mButton1 = (Button) findViewById(R.id.button1);
+        mButton2 = (Button) findViewById(R.id.button2);
+        mSurfaceView = (SurfaceView) findViewById(R.id.surface);
+        mEditText = (EditText) findViewById(R.id.editText1);
+
+
+
+        mSession = SessionBuilder.getInstance()
+                .setCallback(this)
+                .setSurfaceView(mSurfaceView)
+                .setPreviewOrientation(90)
+                .setContext(getApplicationContext())
+                .setAudioEncoder(SessionBuilder.AUDIO_NONE)
+                .setAudioQuality(new AudioQuality(16000, 32000))
+                .setVideoEncoder(SessionBuilder.VIDEO_H264)
+                .setVideoQuality(new VideoQuality(320,240,20,500000))
+                .build();
+
+        mButton1.setOnClickListener(this);
+        mButton2.setOnClickListener(this);
+
+        mSurfaceView.getHolder().addCallback(this);
+
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSession.release();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button2) {
+            // Starts/stops streaming
+            if (!mSession.isStreaming()) {
+                mSession.configure();
+            } else {
+                mSession.stop();
+            }
+            mButton1.setEnabled(false);
+        } else {
+            // Switch between the two cameras
+            mSession.switchCamera();
+        }
+    }
+
+    @Override
+    public void onBitrateUpdate(long bitrate) {
+        Log.d(TAG,"Bitrate: "+bitrate);
+    }
+
+    @Override
+    public void onSessionError(int message, int streamType, Exception e) {
+        mButton1.setEnabled(true);
+        if (e != null) {
+            logError(e.getMessage());
+        }
+    }
+
+    @Override
+    public void onPreviewStarted() {
+        Log.d(TAG,"Preview started.");
+    }
+
+    @Override
+    public void onSessionConfigured() {
+        Log.d(TAG,"Preview configured.");
+        // Once the stream is configured, you can get a SDP formated session description
+        // that you can send to the receiver of the stream.
+        // For example, to receive the stream in VLC, store the session description in a .sdp file
+        // and open it with VLC while streming.
+        Log.d(TAG, mSession.getSessionDescription());
+        mSession.start();
+    }
+
+    @Override
+    public void onSessionStarted() {
+        Log.d(TAG,"Session started.");
+        mButton1.setEnabled(true);
+
+    }
+
+    @Override
+    public void onSessionStopped() {
+        Log.d(TAG,"Session stopped.");
+        mButton1.setEnabled(true);
+
+    }
+
+    /** Displays a popup to report the eror to the user */
+    private void logError(final String msg) {
+        final String error = (msg == null) ? "Error unknown" : msg;
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(error).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {}
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mSession.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mSession.stop();
+    }
+
+
+
+
 
     public void cameraPreviewActivity(View view){
         Intent intent = new Intent(MainActivity.this, CameraPreviewActivity.class);
